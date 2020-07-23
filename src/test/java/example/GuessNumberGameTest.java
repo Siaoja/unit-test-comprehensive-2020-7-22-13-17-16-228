2202551f2b458afa545abbcac89946833aac3612
Siaoja <160692147@qq.com>
2020-07-23 19:52:21 +0800

    [guessNumberGame][spike lu] test: implement test method should_given_1243_when_call_guess_then_return_2A2B

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -5,11 +5,12 @@ import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class GuessNumberGameTest {
 
     @Test
-    void should_given_1234_when_guess_then_return_4A0B() {
+    void should_given_1234_when_call_guess_then_return_4A0B() {
         //given
         Generator mockedAnswerGenerator = Mockito.mock(Generator.class);
         given(mockedAnswerGenerator.generate()).willReturn("1234");
@@ -21,5 +22,20 @@ public class GuessNumberGameTest {
 
         //then
         assertEquals("4A0B",guessResult);
+    }
+
+    @Test
+    void should_given_1243_when_call_guess_then_return_2A2B() {
+        //given
+        Generator mockedAnswerGenerator = Mockito.mock(Generator.class);
+        given(mockedAnswerGenerator.generate()).willReturn("1234");
+        String guessNumber = "1243";
+
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerator);
+        String guessResult = guessNumberGame.guess(guessNumber);
+
+        //then
+        assertEquals("2A2B", guessResult);
     }
 }
